9f95a68c4ce489f09b692112dc5e69d9bf93aa60
Jnn912 <Jnn912@gmail.com>
2024-10-31 20:51:03 -0400

    Fix init file name and update API implementation

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-# Flask
+# 初始化Flask应用
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Sql
+# 初始化数据库
 db = SQLAlchemy(app)
 
-# models
+# 导入路由和模型
 from app import routes, models
 
-# creat 
+# 创建数据库表
 with app.app_context():
     db.create_all()
